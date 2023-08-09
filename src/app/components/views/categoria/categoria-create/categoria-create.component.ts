8afaa1717d94f29490f8dce5d09333880678d334
VictorEmanuel08 <victorem3181@gmail.com>
2023-08-09 17:52:39 -0300

    Metodo Create implementado para Categoria

--- src/app/components/views/categoria/categoria-create/categoria-create.component.ts
+++ src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CategoriaService } from "../categoria.service";
+import { Categoria } from "../categoria.model";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-categoria-create',
-  templateUrl: './categoria-create.component.html',
-  styleUrls: ['./categoria-create.component.css']
+  selector: "app-categoria-create",
+  templateUrl: "./categoria-create.component.html",
+  styleUrls: ["./categoria-create.component.css"],
 })
 export class CategoriaCreateComponent {
+  categoria: Categoria = {
+    nome: "",
+    descricao: "",
+  };
 
+  constructor(private service: CategoriaService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  create(): void {
+    this.service.create(this.categoria).subscribe(
+      (resposta) => {
+        this.router.navigate(["categorias"]);
+        this.service.mensagem("Categoria criada com sucesso!");
+      },
+      (err) => {
+        for (let i = 0; i < err.error.erros.length; i++) {
+          this.service.mensagem(err.error.erros[i].message);
+        }
+      }
+    );
+  }
 }
