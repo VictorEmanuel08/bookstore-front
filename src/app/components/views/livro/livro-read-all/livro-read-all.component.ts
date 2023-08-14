381fcd5a03fcf0a0cd9483c02a22a9bf0b5b3ddc
VictorEmanuel08 <victorem3181@gmail.com>
2023-08-14 17:28:10 -0300

    Componente Livros implementado

---
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+import { Livro } from "../livro.model";
+import { LivroService } from "../livro.service";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "app-livro-read-all",
+  templateUrl: "./livro-read-all.component.html",
+  styleUrls: ["./livro-read-all.component.css"],
+})
+export class LivroReadAllComponent {
+  id_cat: String = "";
+
+  livros: Livro[] = [];
+
+  displayedColumns: string[] = ["id", "titulo", "livros", "acoes"];
+
+  constructor(private service: LivroService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAllByCategoria(this.id_cat).subscribe((resposta) => {
+      this.livros = resposta;
+    });
+  }
+}
